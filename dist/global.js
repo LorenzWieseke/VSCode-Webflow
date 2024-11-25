b0f9a707b1d650398d0c8dd5c3fd25a06a2e437e
Lorenz Wieseke <lorenz@wieseke.com>
2024-11-25 20:12:59 +0100

    spline buttons

--- dist/global.js
+++ dist/global.js
@@ -1,1 +1,44 @@
-!function(o,e){"object"==typeof exports&&"object"==typeof module?module.exports=e():"function"==typeof define&&define.amd?define("global",[],e):"object"==typeof exports?exports.global=e():o.global=e()}(this,(()=>(console.log("Hello World"),{})));
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("global", [], factory);
+	else if(typeof exports === 'object')
+		exports["global"] = factory();
+	else
+		root["global"] = factory();
+})(this, () => {
+return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const bindBtnsToSpline = (splineViewer) => {\r\n    const btns = document.querySelectorAll(`.spline-btn`);\r\n    const app = splineViewer._spline;\r\n\r\n    // Get all variables in the Spline scene\r\n    const variables = app.getVariables();\r\n    console.log('Spline Variables:', variables);\r\n\r\n    btns.forEach((btn) => {\r\n        btn.addEventListener(\"click\", (event) => {\r\n            event.preventDefault();\r\n\r\n            // Iterate over all attributes of the button\r\n            for (let attr of btn.attributes) {\r\n                const attrName = attr.name;\r\n                const attrValue = attr.value;\r\n\r\n                // Check if the attribute name matches a variable in the Spline scene\r\n                if (variables.hasOwnProperty(attrName)) {\r\n                    console.log(`Setting variable ${attrName} to ${attrValue}`);\r\n                    app.setVariables({ [attrName]: parseInt(attrValue) });\r\n                } \r\n            }\r\n        });\r\n    });\r\n};\r\n\r\n// Initialize the Spline viewer\r\nconst initializeSplineViewer = () => {\r\n    const splineViewer = document.querySelector('spline-viewer');\r\n\r\n    splineViewer.addEventListener(\"load-complete\",() => {\r\n        console.log(\"Spline Viewer loaded\");\r\n        bindBtnsToSpline(splineViewer);\r\n    });\r\n\r\n};\r\n\r\n\r\ninitializeSplineViewer();\r\n\n\n//# sourceURL=webpack://vs-code/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
+const bindBtnsToSpline = (splineViewer) => {
+    const btns = document.querySelectorAll(`.spline-btn`);
+    const app = splineViewer._spline;
+
+    // Get all variables in the Spline scene
+    const variables = app.getVariables();
+    console.log('Spline Variables:', variables);
+
+    btns.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            // Iterate over all attributes of the button
+            for (let attr of btn.attributes) {
+                const attrName = attr.name;
+                const attrValue = attr.value;
+
+                // Check if the attribute name matches a variable in the Spline scene
+                if (variables.hasOwnProperty(attrName)) {
+                    console.log(`Setting variable ${attrName} to ${attrValue}`);
+                    app.setVariables({ [attrName]: parseInt(attrValue) });
+                } 
+            }
+        });
+    });
+};
+
 // Initialize the Spline viewer
 const initializeSplineViewer = () => {
     const splineViewer = document.querySelector('spline-viewer');
 
     splineViewer.addEventListener("load-complete",() => {
         console.log("Spline Viewer loaded");
+        bindBtnsToSpline(splineViewer);
     });
 
 };
 
 
-// initializeSplineViewer();
-
-console.log("Hello World");
+initializeSplineViewer();
